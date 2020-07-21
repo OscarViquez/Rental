4fb58f4162030b50346598269899d31e55ef0054
OscarViquez <55415043+OscarViquez@users.noreply.github.com>
2020-07-20 22:20:03 -0400

    added more css and visual changes

---
+++ app.py
@@ -0,0 +1,14 @@
+var1 = '1'
+var2 = '2'
+
+firstvVal = 1
+secVal = 2
+
+
+# print(var1 + var2)
+
+print(var1 + '+' + var2)
+
+print(firstvVal + secVal)
+
+print(firstvVal * secVal)
--- client/public/js/index.js
+++ client/public/js/index.js
@@ -75,4 +75,5 @@ navLinks.forEach(link => {
 
 
 
+
                                                                                                                                                         
